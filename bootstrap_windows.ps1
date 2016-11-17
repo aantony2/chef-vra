d601dc253632f25487b6a3e3088378b53cf1a423
Ramya Kailas <ramyakailas@users.noreply.github.com>
2016-11-17 11:08:51 -0500

    Update bootstrap_windows.ps1

--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -6,9 +6,9 @@ $chef_server_url = $args[0]
 Add-Content C:\chef\client.rb -Value "log_level       :info"
 Add-Content C:\chef\client.rb -Value "log_location    STDOUT"
 Add-Content C:\chef\client.rb -Value "chef_server_url    $args[2]"
-Add-Content C:\chef\client.rb -Value "validation_key  \`'/etc/chef/validation.pem\`'"
+Add-Content C:\chef\client.rb -Value "validation_key  \`'/etc/chef/validation.pem'"
 
-Add-Content C:\chef\client.rb -Value "validation_client_name \`'onecloud-validator`'"
+Add-Content C:\chef\client.rb -Value "validation_client_name \`'onecloud-validator'"
 Add-Content C:\chef\client.rb -Value "ssl_verify_mode `:verify_none"
 
 new-item -path C:\chef\firstboot.json -type file
