7a8a6bae6ca4b145c29cb8cbf4c9ca5fa064e82e
Ramya Kailas <ramyakailas@users.noreply.github.com>
2016-11-17 13:33:17 -0500

    Update bootstrap_windows.ps1

--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -1,14 +1,14 @@
-new-item -path C:\chef\client.rb -type file
+new-item -path /Users/rkailas/Mckesson/client.rb -type file
 
-$file =  C:\chef\client.rb
-$chef_server_url = $args[0]
 
-Add-Content C:\chef\client.rb -Value "log_level       :info"
-Add-Content C:\chef\client.rb -Value "log_location    STDOUT"
-Add-Content C:\chef\client.rb -Value "chef_server_url    $args[2]"
-Add-Content C:\chef\client.rb -Value "validation_key  \`'/etc/chef/validation.pem'"
+Add-Content /Users/rkailas/Mckesson/client.rb -Value "log_level       :info"
+Add-Content /Users/rkailas/Mckesson/client.rb -Value "log_location    STDOUT"
+Add-Content /Users/rkailas/Mckesson/client.rb -Value "chef_server_url    $($args[0])"
+Add-Content /Users/rkailas/Mckesson/client.rb -Value "validation_key  '/etc/chef/validation.pem'"
 
-Add-Content C:\chef\client.rb -Value "validation_client_name \`'onecloud-validator'"
-Add-Content C:\chef\client.rb -Value "ssl_verify_mode `:verify_none"
+Add-Content /Users/rkailas/Mckesson/client.rb -Value "validation_client_name 'onecloud-validator'"
+Add-Content /Users/rkailas/Mckesson/client.rb -Value "ssl_verify_mode `:verify_none"
 
-new-item -path C:\chef\firstboot.json -type file
+new-item -path /Users/rkailas/Mckesson/firstboot.json -type file
+
+Add-Content /Users/rkailas/Mckesson/firstboot.json -Value '{"run_list": ["recipe[getting-started]"]}'
