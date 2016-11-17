87cc1da8b463c0168eb74daae0fb8747f4f9cec0
Ramya Kailas <ramyakailas@users.noreply.github.com>
2016-11-17 14:52:06 -0500

    Update bootstrap_windows.ps1

--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -1,6 +1,10 @@
-new-item -path /Users/rkailas/Mckesson/client.rb -type file
+if (!(Test-Path "/Users/rkailas/Mckesson/client.rb"))
+{
+  new-item -path /Users/rkailas/Mckesson/client.rb -type file
+}
 
-
+else
+{
 Add-Content /Users/rkailas/Mckesson/client.rb -Value "log_level       :info"
 Add-Content /Users/rkailas/Mckesson/client.rb -Value "log_location    STDOUT"
 Add-Content /Users/rkailas/Mckesson/client.rb -Value "chef_server_url    $($args[0])"
@@ -8,10 +12,16 @@ Add-Content /Users/rkailas/Mckesson/client.rb -Value "validation_key  '/etc/chef
 
 Add-Content /Users/rkailas/Mckesson/client.rb -Value "validation_client_name 'onecloud-validator'"
 Add-Content /Users/rkailas/Mckesson/client.rb -Value 'ssl_verify_mode :verify_none'
+}
 
+if (!(Test-Path "/Users/rkailas/Mckesson/first-boot.json"))
+{
 new-item -path /Users/rkailas/Mckesson/first-boot.json -type file
-
+}
+else
+{
 Add-Content /Users/rkailas/Mckesson/first-boot.json -Value "{`"run_list`": [`"recipe[$($args[1])]`"]}"
+}
 
 $FIRSTBOOTJSON = "/Users/rkailas/Mckesson/first-boot.json"
 
